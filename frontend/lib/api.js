9b1a0015902d355cb0e8754052f37ae674774154
Mayankkumar0509 <23f1003168@ds.study.iitm.ac.in>
2025-11-17 21:25:26 +0530

    commit

--- frontend/lib/api.js
+++ frontend/lib/api.js
@@ -1,1 +1,19 @@
+import axios from "axios";
+import { getToken } from "./auth";
 
+const API_BASE = process.env.NEXT_PUBLIC_API_BASE_URL;
+
+const api = axios.create({
+  baseURL: API_BASE,
+  headers: {
+    "Content-Type": "application/json"
+  }
+});
+
+api.interceptors.request.use((config) => {
+  const token = getToken();
+  if (token) config.headers.Authorization = `Bearer ${token}`;
+  return config;
+});
+
+export default api;
