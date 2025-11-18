70d31229a7df65fb8e394342ab983f0ee0cf9f7d
Mayankkumar0509 <23f1003168@ds.study.iitm.ac.in>
2025-11-18 15:12:24 +0530

    commit

--- frontend/pages/_app.js
+++ frontend/pages/_app.js
@@ -1,1 +1,14 @@
 
+import "../styles/globals.css";
+import DarkModeToggle from "../components/DarkModeToggle";
+import NavBar from "../components/NavBar";
+
+export default function App({ Component, pageProps }) {
+  return (
+    <div className="min-h-screen bg-gray-50 dark:bg-gray-900 transition">
+      <NavBar />
+      <Component {...pageProps} />
+      <DarkModeToggle />
+    </div>
+  );
+}
