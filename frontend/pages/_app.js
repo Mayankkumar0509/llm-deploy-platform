0955a02c76fef1d299853881a1ce97762069de05
Mayankkumar0509 <23f1003168@ds.study.iitm.ac.in>
2025-11-20 00:38:36 +0530

    Update _app.js

--- frontend/pages/_app.js
+++ frontend/pages/_app.js
@@ -1,9 +1,8 @@
 import "../styles/globals.css";
-import { useEffect } from "react";
 import NavBar from "../components/NavBar";
+import { useEffect } from "react";
 
 export default function MyApp({ Component, pageProps }) {
-
   useEffect(() => {
     console.log("App mounted");
   }, []);
