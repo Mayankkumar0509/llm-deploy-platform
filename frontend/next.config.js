24bd6de4a23440602c4d6eee80262b22a3eb7d49
Mayankkumar0509 <23f1003168@ds.study.iitm.ac.in>
2025-11-20 00:47:51 +0530

    Update next.config.js

--- frontend/next.config.js
+++ frontend/next.config.js
@@ -1,17 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-
-  experimental: {
-    optimizePackageImports: [],
-  },
-
   pageExtensions: ["js", "jsx"],
-
-  output: "standalone",
-
-  trailingSlash: true,
+  output: "standalone"
 };
-
 module.exports = nextConfig;
-
